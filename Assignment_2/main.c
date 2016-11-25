7bc2236cc3a9b03ed0a9cd15f5075ed143c1adf0
Lawernce Kay <lkay9495@hotmail.com>
2016-11-25 21:42:38 +0900

    Reorganized Directory Structure & Initial Commit for Assignemnt 2

---
+++ Assignment_2/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <pthread.h>
+#include "buffer.h"
+
+/* the buffer */
+
+buffer item buffer[BUFFER SIZE];
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
+
+int insert item(buffer item item) {
+	/* insert item into buffer
+	return 0 if successful, otherwise
+	return -1 indicating an error condition */
+}
+int remove item(buffer item *item){
+	/* remove an object from buffer
+	placing it in item
+	return 0 if successful, otherwise
+	return -1 indicating an error condition */
+}
